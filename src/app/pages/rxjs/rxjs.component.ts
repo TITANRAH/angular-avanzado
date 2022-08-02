94b0a8cf01d1382d876d2c37897072a82d16d180
sergio miranda <sergiomiranda@sergios-MacBook-Pro.local>
2022-08-02 00:53:38 -0400

    rutas etc

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,66 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, retry, interval, take, map, filter, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+  selector: "app-rxjs",
+  templateUrl: "./rxjs.component.html",
+  styleUrls: ["./rxjs.component.css"],
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy{
+
+  intervalSubs!: Subscription
 
   constructor() {
-    
-    let i = -1;
+    // aca me suscribo a ese valor emitido y me imprime el valor cada vez que se genere
+    // cada vez que salga y entre a este componente se repetira el set interval por eso es bueno suscribirse
+    // obs$.subscribe(resp => {
+    //   console.log(resp);
+    // });
 
-    const obs$ = new Observable( observer => {
+    //el pipe puede transformar la informacion o reintentar a realizar su funcion
+    // el retry intenta una y otra vez
+    // this.retornaObservable().pipe(
 
+    //   retry(2)
 
-      const interval = setInterval(() => {
+    // ).subscribe({
 
-        // inicializo en menos 1 y aca en el interval lo aumento en 1
-        // luego uso la variable observer .net para emitir el valor i que se genera cada 1 segundo
-       i++
-       observer.next(i);
+    //   next: valor => console.log('Subs:', valor),
+    //   error: err => console.warn('Error', err),
+    //   complete: () => console.info('Obs terminado')
 
-       if ( i == 4){
+    // });
 
-        clearInterval(interval)
-        observer.complete()
-       }
+   this.intervalSubs =  this.retornaIntervalo().subscribe(console.log);
+  }
+  
 
-       if ( i == 2){
-        console.log('i = 2.... error');
-        
-        observer.error( 'i llego al valor de 2')
-       }
-        
-      },1000)
-
-    })
-      // aca me suscribo a ese valor emitido y me imprime el valor cada vez que se genere
-      // cada vez que salga y entre a este componente se repetira el set interval por eso es bueno suscribirse
-      // obs$.subscribe(resp => {      
-      //   console.log(resp);
-      // });
-
-      //el pipe puede transformar la informacion o reintentar a realizar su funcion
-      // el retry intenta una y otra vez 
-      obs$.pipe(
-
-        retry(2)
-
-      ).subscribe({
-       
-        next: valor => console.log('Subs:', valor), 
-        error: err => console.warn('Error', err),
-        complete: () => console.info('Obs terminado') 
-        
-      });
-
-
-   }
-
-  ngOnInit(): void {
+  retornaIntervalo(): Observable<any> {
+    // take solo toma el o los valores deseados en este caso 4 emisiones del observable
+    // el taje toma los 4 valores el map toma la informacion y la tranformo en el valor + 1 por lo que ahora
+    // empieza del 1 y no del 0 el conteo del interval
+    // si yo quiisera mostrar un hola mundo en el console del map solo eso mostrara el hola mundo 4 veces
+    // el map me permite extraer la infomracion que solo ocupare de una respuesta de una api
+    return interval(100).pipe(
+      map(valor => valor + 1), //el metodo filtra los pares y los deja pasar de lo contrario se detiene
+      filter(valor => (valor % 2 == 0 ? true : false)),
+      take(10),
+      );
   }
 
+  retornaObservable(): Observable<any> {
+    let i = -1;
+
+    const obs$ = new Observable((observer) => {
+      const interval = setInterval(() => {
+        // inicializo en menos 1 y aca en el interval lo aumento en 1
+        // luego uso la variable observer .net para emitir el valor i que se genera cada 1 segundo
+        i++;
+        observer.next(i);
+
+        if (i == 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i == 2) {
+          console.log("i = 2.... error");
+
+          observer.error("i llego al valor de 2");
+        }
+      }, 1000);
+    });
+
+    return obs$;
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe()
+  }
 }
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  titulo!: string;
+  tituloSubs$!: Subscription
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) {
+      // esta forma usa route para el efecto de leer parametros en las rutas          
+     // console.log(route.snapshot.children[0].data);
+      
+
+
+
+  //  this.tituloSubs$ = this.getArgumentosRuta().subscribe(data => {
+  //   console.log(data);
+  //   this.titulo = data['titulo']
+
+  //   //esto es para las pestañas de las paginas
+  //   document.title = `AdminPro - ${this.titulo}`
+    
+  // })
+
+   }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe
+  }
 
   ngOnInit(): void {
+  }
+
+  getArgumentosRuta(){
+     // primero le di a las rutas en pages routing modules el atributo data con la propiedad titulo en la ruta
+    // despues le hice un console.log al evento suscribiendome para ver que tipo de evento era
+    // luego inyecto el router el cual tiene una propiedad llamada events, uso pipes para filtrar
+    // el evento que trae finalmente la propiedad titulo
+   return this.router.events
+    .pipe(
+      filter( (event:any )=> event instanceof ActivationEnd),
+      filter( (event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map( (event:ActivationEnd)=> event.snapshot.data)
+    )
   }
 
 }
